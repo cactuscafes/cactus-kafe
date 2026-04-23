28a30b06965e32b904f2d52439e71ac5827750c3
cactuscafes <batuhanbulut@cactuscafes.com>
2026-04-24 01:38:59 +0300

    fsm hook: odeme_tipi eklendi

--- rapor-hook-fsm.js
+++ rapor-hook-fsm.js
@@ -1,35 +1,15 @@
 (function(){
-  var API='https://cactus-rapor-api.batuhanbulut.workers.dev';
-  var SUBE='fsm';
-  var LK='cactus_satis_fsmlar';
-  var _sonKayit=0;
-
-  function d1Gonder(satis){
-    var tarih=satis.tarihISO||new Date().toISOString().split('T')[0];
-    (satis.urunDetay||[]).forEach(function(u){
-      fetch(API+'/rapor/kaydet',{
-        method:'POST',
-        headers:{'Content-Type':'application/json'},
-        body:JSON.stringify({sube:SUBE,tarih:tarih,urun_adi:u.ad,kategori:u.tip||'',adet:u.adet||1,birim_fiyat:u.fiyat||0})
-      }).catch(function(){});
+  var API='https://cactus-rapor-api.batuhanbulut.workers.dev',SUBE='fsm',LK='cactus_satis_fsmlar',_son=0;
+  function gonder(s){
+    var tarih=s.tarihISO||new Date().toISOString().split('T')[0];
+    var tip=s.odeme_tipi==='Nakit'?'nakit':'kredi';
+    (s.urunDetay||[]).forEach(function(u){
+      fetch(API+'/rapor/kaydet',{method:'POST',headers:{'Content-Type':'application/json'},
+      body:JSON.stringify({sube:SUBE,tarih:tarih,urun_adi:u.ad,kategori:u.tip||'',adet:u.adet||1,birim_fiyat:u.fiyat||0,odeme_tipi:tip})}).catch(function(){});
     });
   }
-
   function kontrol(){
-    try{
-      var s=JSON.parse(localStorage.getItem(LK)||'[]');
-      if(s.length>_sonKayit){
-        for(var i=_sonKayit;i<s.length;i++) d1Gonder(s[i]);
-        _sonKayit=s.length;
-      }
-    }catch(e){}
+    try{var s=JSON.parse(localStorage.getItem(LK)||'[]');if(s.length>_son){for(var i=_son;i<s.length;i++)gonder(s[i]);_son=s.length;}}catch(e){}
   }
-
-  window.addEventListener('load',function(){
-    setTimeout(function(){
-      var s=JSON.parse(localStorage.getItem(LK)||'[]');
-      _sonKayit=s.length;
-      setInterval(kontrol,2000);
-    },1000);
-  });
+  window.addEventListener('load',function(){setTimeout(function(){var s=JSON.parse(localStorage.getItem(LK)||'[]');_son=s.length;setInterval(kontrol,2000);},1000);});
 })();
